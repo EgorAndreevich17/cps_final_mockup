8ca785634440abb269b7f01b850667e97452c20e
EgorAndreevich17 <theonlyegor1709@gmail.com>
2024-07-01 23:02:51 +0300

    finished brand-slider

--- js/button.js
+++ js/button.js
@@ -1,12 +1,12 @@
-let button = document.querySelector(".nav__show-more-button");
-let wrapper = document.querySelector(".nav-wrapper");
+let button = document.querySelector(".section__show-more-button");
+let wrapper = document.querySelector(".brand-slider__list");
 
 button.addEventListener("click", function (evt) {
     evt.preventDefault();
-    button.classList.toggle("nav__show-more-button--reversed");
-    wrapper.classList.toggle("nav-wrapper--preview");
+    button.classList.toggle("section__show-more-button--reversed");
+    wrapper.classList.toggle("brand-slider__list--preview");
 
-    if (wrapper.classList.contains("nav-wrapper--preview")) {
+    if (wrapper.classList.contains("brand-slider__list--preview")) {
         button.textContent = "Показать все";
     } else {
         button.textContent = "Скрыть";
